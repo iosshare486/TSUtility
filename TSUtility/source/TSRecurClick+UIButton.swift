84f96efe30461f083dc03d86c02e1a59df9529d0
Hongli183 <261930323@qq.com>
2018-08-17 15:59:25 +0800

    修复正常按钮点击crash问题

--- TSUtility/source/TSRecurClick+UIButton.swift
+++ TSUtility/source/TSRecurClick+UIButton.swift
@@ -12,9 +12,9 @@ import UIKit
 private var key: Void?
 public extension UIButton {
     
-    var time_interval : TimeInterval{
+    var time_interval : TimeInterval? {
         get {
-            return (objc_getAssociatedObject(self, &key) as? TimeInterval)!
+            return (objc_getAssociatedObject(self, &key) as? TimeInterval) ?? nil
         }
         set(newValue) {
             objc_setAssociatedObject(self, &key, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
@@ -34,9 +34,17 @@ public extension UIButton {
     //手指抬起
     open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         //根据需要设置按钮是否可以点击
-        if self.time_interval > 0 {
+        
+        guard let time_interval = self.time_interval else {
+            
+            super.touchesEnded(touches, with: event)
+            return
+            
+        }
+        
+        if time_interval > 0 {
             self.isUserInteractionEnabled = false
-            self.perform(#selector(restoreUserActivityStateOfButton), with: nil, afterDelay: self.time_interval)
+            self.perform(#selector(restoreUserActivityStateOfButton), with: nil, afterDelay: time_interval)
         }
         super.touchesEnded(touches, with: event)
     }
@@ -45,4 +53,3 @@ public extension UIButton {
         self.isUserInteractionEnabled = true
     }
 }
-
